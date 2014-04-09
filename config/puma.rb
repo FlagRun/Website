df3588ae52a598abddce3c7d239d27b998b31bde
Liothen <liothen@flagrun.net>
2014-04-08 20:38:28 -0700

    puma config added

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env puma
+directory   '/var/www/flagrun.net/current'
+enviroment  'production'
+
+
+threads 8,32
+workers 4
+preload_app!
+
+# Daemonize
+daemonize true
+pidfile     '/var/www/flagrun.net/run/puma/puma.pid'
+state_path  '/var/www/flagrun.net/run/puma/puma.state'
+bind        'unix:///var/www/flagrun.net/run/puma.sock'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
